d52dfb131dfaf6d6c44c4e9b44872f619ae9c1e9
GhaydA-AlQudah <ghayda.alqudah@gmail.com>
2024-02-24 04:31:14 -0800

    added new model

--- snippets/models.py
+++ snippets/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Diabetes(models.Model):
+    Glocuse = models.FloatField(max_length =10)
+    BMI = models.FloatField(max_length =10)
+    Pressure = models.FloatField(max_length=10)
+
+    def __str__(self) :
+        return f' ${self.Glocuse}'
